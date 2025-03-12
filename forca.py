c09725e3826e8e6181f77765db9b9b767b50998b
gpires97 <162461724+gpires97@users.noreply.github.com>
2025-03-12 10:55:14 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,29 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo de Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while(not acertou and not enforcou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index]= letra
+            index = index + 1
+
+        print(letras_acertadas)
+
+    print("Fim do jogo!")
+
+
+if(__name__ == "__main__"):
+    jogar()
