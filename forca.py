0aa8b6d86a31e2bcd2b66e6806461b9d1d9b6c61
gpires97 <162461724+gpires97@users.noreply.github.com>
2025-03-12 11:55:28 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,7 +1,7 @@
 def jogar():
 
     print("*********************************")
-    print("***Bem vindo ao jogo de Forca!***")
+    print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
     palavra_secreta = "banana".upper()
@@ -13,28 +13,28 @@ def jogar():
 
     print(letras_acertadas)
 
-    while(not acertou and not enforcou):
+    while (not acertou and not enforcou):
 
-        chute = input("Qual a letra? ")
+        chute = input("Qual letra? ")
         chute = chute.strip().upper()
 
-        if(chute in palavra_secreta):
+        if (chute in palavra_secreta):
             index = 0
             for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_acertadas[index]= letra
-                index =+ 1
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
         else:
-            erros =+ 1
+            erros += 1
 
         enforcou = erros == 6
-        acertou =  "_" not in letras_acertadas 
-             
+        acertou = "_" not in letras_acertadas
+
         print(letras_acertadas)
-    if(acertou):
+    if (acertou):
         print("Você ganhou!")
     else:
         print("Você perdeu!")
-    print("Fim do jogo!")
+    print("Fim do jogo")
 if(__name__ == "__main__"):
     jogar()
